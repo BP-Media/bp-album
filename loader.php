623dfadca9ccbd66fbe1929cffbdad9b3f3849a4
fanquake <fanquake@gmail.com>
2011-11-14 14:27:55 +0000

    Adding the new Russian translations

--- loader.php
+++ loader.php
@@ -4,9 +4,9 @@ Plugin Name: BuddyPress Album
 Plugin URI: http://code.google.com/p/buddypress-media/
 Description: Photo Albums for BuddyPress. Includes Posts to Wire, Member Comments, and Gallery Privacy Controls. Works with the current BuddyPress theme and includes Easy To Skin Templates.
 Version: 0.1.8.11
-Revision Date: September 21, 2011
+Revision Date: November 14, 2011
 Requires at least: 3.2
-Tested up to: WP 3.2.1, BP 1.5, PHP 5.3.6
+Tested up to: WP 3.2.1, BP 1.5.1, PHP 5.3.6
 License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
 Author: The BP-Media Team
 Author URI: http://code.google.com/p/buddypress-media/people/list
