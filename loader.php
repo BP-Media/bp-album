a3b2409bfa2d2b672d45a5210eb0f18ea3e39a4d
foxly <carlroett@gmail.com>
2012-12-15 04:08:20 -0700

    Updating versioning info

--- loader.php
+++ loader.php
@@ -4,12 +4,12 @@ Plugin Name: BP-Album
 Plugin URI: http://code.google.com/p/buddypress-media/
 Description: Photo Albums for BuddyPress. Includes Posts to Wire, Member Comments, and Gallery Privacy Controls. Works with the current BuddyPress theme and includes Easy To Skin Templates.
 Version: 0.1.8.12
-Revision Date: August 27, 2012
-Requires at least: 3.2
-Tested up to: WP 3.4.1, BP 1.6.1, PHP 5.4.4
+Revision Date: December 15, 2012
+Requires at least: 3.5
+Tested up to: WP 3.5, BP 1.6.1, PHP 5.4.4
 License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
 Author: The BP-Media Team
-Author URI: http://code.google.com/p/buddypress-media/people/list
+Author URI: https://github.com/BP-Media
 Network: True
 Text Domain: bp-album
 */
